f6824f8b87d6f27625704f45cc1a7a466828a06d
Matt1479 <gitmatt1479@gmail.com>
2025-06-06 16:36:50 +0200

    Refactor(bot.py): Increase timeout of requests to 5 seconds, use Plotter to plot historic data, add more error messages.

--- bot.py
+++ bot.py
@@ -4,36 +4,39 @@
 from Coinbase import Coinbase
 import discord
 from discord.ext import commands
-import json
+import helpers
 import requests
 
 # Open token.txt in read mode, store the reference inside of
 # token_file variable
-with open('token.txt', 'r') as token_file:
+with open("token.txt", "r") as token_file:
     # Read a single line and store it inside of token
     token = token_file.readline()
 
 # Intents allow the bot to "subscribe" or "listen" to specific events
 intents = discord.Intents.all()
 # Create an instance of Bot class
-bot = commands.Bot(command_prefix='.', intents=intents)
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 # coinbase gets a reference to a new instance of Coinbase class
-coinbase = Coinbase()
+coinbase = Coinbase(timeout=5)
+
+# Create an instance of Plotter
+plotter = helpers.Plotter()
 
 
 @bot.event
 async def on_ready():
     """Called when the bot has finished logging in and
     setting things up."""
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('--------')
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("--------")
 
 
 @bot.command()
 async def echo(context: commands.Context, *messages):
     """Repeats a message (or messages) back to the user."""
-    separator = ' '
+    separator = " "
     # Context refers to things like: the user invoking the command, etc.
     await context.send(separator.join(messages))
 
@@ -41,11 +44,11 @@ async def echo(context: commands.Context, *messages):
 @bot.command()
 async def uselessfact(context: commands.Context):
     """Basic HTTP GET request."""
-    response = requests.get('https://uselessfacts.jsph.pl/random.json')
+    response = requests.get("https://uselessfacts.jsph.pl/random.json")
 
     if response.status_code == 200:
         json_response = response.json()
-        await context.send(json_response['text'])
+        await context.send(json_response["text"])
 
 
 @bot.command()
@@ -54,11 +57,9 @@ async def get_crypto_price(context: commands.Context, *messages):
         await context.send("Usage: .get_crypto_price crypto_currency base_currency")
     else:
         price = coinbase.get_crypto_price(
-            currency_pair = {
-                "base": messages[1],
-                "crypto": messages[0]
-            })
-        
+            currency_pair={"base": messages[1], "crypto": messages[0]}
+        )
+
         if price:
             await context.send(f"{price['amount']:.02f}")
         else:
@@ -68,23 +69,35 @@ async def get_crypto_price(context: commands.Context, *messages):
 @bot.command()
 async def get_historic_prices(context: commands.Context, *messages):
     if len(messages) != 4:
-        await context.send("Usage: .get_historic_prices crypto_currency base_currency start_date end_date")
+        await context.send(
+            "Usage: .get_historic_prices crypto_currency base_currency start_date end_date"
+        )
         await context.send("start_date, end_date: YYYY-MM-DD")
     else:
-        historic_prices = coinbase.get_historic_prices({
-            "start_date": messages[2],
-            "end_date": messages[3],
-            "currency_pair": {
-                "base": messages[1],
-                "crypto": messages[0]
+        historic_prices = coinbase.get_historic_prices(
+            {
+                "start_date": messages[2],
+                "end_date": messages[3],
+                "currency_pair": {"base": messages[1], "crypto": messages[0]},
             }
-        })
-        
+        )
+
         if historic_prices:
-            await context.send(json.dumps(historic_prices, indent=4))
+            plotter.plot(
+                helpers.list_dict_to_list(historic_prices, "date"),
+                helpers.list_dict_to_list(historic_prices, "amount"),
+            )
+            plotter.setup("Date", "Amount", "Historic Prices", True, False)
+
+            plotter.save("out.png")
+            await context.send("Fetching data...", file=discord.File("out.png"))
+            plotter.clear()
         else:
-            await context.send("Usage: .get_historic_prices crypto_currency base_currency start_date end_date")
+            await context.send(
+                "Usage: .get_historic_prices crypto_currency base_currency start_date end_date"
+            )
             await context.send("start_date, end_date: YYYY-MM-DD")
+            await context.send("start_date >= end_date and months <= 25")
 
 
 # Run the bot with the API token
